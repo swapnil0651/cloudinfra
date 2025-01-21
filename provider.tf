bac7e809e762d8f89250e5ac33331191cc81cc6a
swapnil <swapnilraj0651@gmail.com>
2025-01-21 22:57:14 +0530

    added bug fixes

--- provider.tf
+++ provider.tf
@@ -5,24 +5,19 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  
 }
+
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
-  
+}
 
-}
-resource "aws_route_table_association" "public_route_association" {
-  subnet_id = aws_subnet.subnet.id
-  route_table_id = aws_route_table.public_route_table.id
-  
-}
 resource "aws_subnet" "subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -33,19 +28,26 @@ resource "aws_subnet" "subnet" {
   }
 }
 
+resource "aws_route_table_association" "public_route_association" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 resource "aws_security_group" "allow_http" {
   name_prefix = "allow_http"
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress  {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -62,6 +64,7 @@ resource "aws_security_group" "allow_http" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "Mysg"
   }
@@ -72,20 +75,19 @@ resource "aws_instance" "nextjs_instance" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.subnet.id
   security_groups = [aws_security_group.allow_http.id]
-  key_name = "newswapkey"
-
+  key_name      = "newswapkey"
 
   user_data = <<-EOF
-              curl https://gist.githubusercontent.com/swapnil0651/2601df1936374dcf4475a5e2030eaebb/raw/be45c639cdafefb7c8fe5b1643406e9ed6978242/setup.sh | bash
-              
-            
-              EOF
+  #!/bin/bash
+  curl -s https://gist.githubusercontent.com/swapnil0651/2601df1936374dcf4475a5e2030eaebb/raw/be45c639cdafefb7c8fe5b1643406e9ed6978242/setup.sh | bash
+EOF
+
 
   tags = {
     Name = "NextjsApp"
   }
 }
+
 output "ec2_public_ip" {
   value = aws_instance.nextjs_instance.public_ip
-  
 }
